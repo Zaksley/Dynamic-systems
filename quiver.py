5544f253e5ac733253f94ee61ac53429b1a3b2b4
MEDINA Enzo <emedina@enseirb-matmeca.fr>
2021-05-05 17:07:47 +0200

    Quiver, might be ok now

--- quiver.py
+++ quiver.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from math import sqrt
+from math import *
 
 def function_test(x, y):
     return np.array([  -2*x*(y[0]**2)  ])
 
+def function_test_1D(x, y):
+    return np.array([ y[0]/(1+x**2) ])
 
 def draw_tangente_field_1D(F):
     X = np.arange(-2, 2, 0.1)
@@ -17,7 +19,7 @@ def draw_tangente_field_1D(F):
     for i in range(len(X)):
         for j in range(len(Y)):
             x = X[i]
-            y = Y[len(Y)-1-j]
+            y = Y[j]
 
             value = F(x, np.array([y]) )[0]
 
@@ -26,8 +28,42 @@ def draw_tangente_field_1D(F):
             # z = z/norme(z)
             # U = 1/norme(z)
 
-            U[i][j] = (1 / sqrt(1 + value**2 ))
-            V[i][j] = U[i][j] * value
+            U[j][i] = (1 / sqrt(1 + value**2 ))
+            V[j][i] = U[j][i] * value
+
+    print(U)
+    print(V)
+
+    fig, ax = plt.subplots()
+    q = ax.quiver(X, Y, U, V)
+    #ax.quiverkey(q, X=0.3, Y=1.1, U=5,
+    #        label='Quiver key, length = 10', labelpos='E')
+
+    plt.show()
+
+
+def function_test_2d(x, y): 
+    return np.array([ -y, x ])
+
+
+def draw_tangente_field_2D(F):
+    X = np.arange(-2, 2, 0.1)
+    Y = np.arange(-2, 2, 0.1)
+
+    U = np.zeros( (len(X), len(Y)) )
+    V = np.zeros( (len(X), len(Y)) )
+
+    # Creation U & V matrices (Vectors)
+    for i in range(len(X)):
+        for j in range(len(Y)):
+            x = X[i]
+            y = Y[j]
+
+            value = F(x, y)
+            norme = sqrt(value[0]**2 + value[1]**2)
+
+            U[j][i] = ( value[0] / norme)
+            V[j][i] = ( value[1] / norme)
 
     print(U)
     print(V)
@@ -41,5 +77,5 @@ def draw_tangente_field_1D(F):
 
 
 
-
-draw_tangente_field_1D(function_test)
+draw_tangente_field_1D(function_test_1D)
+#draw_tangente_field_2D(function_test_2d)
